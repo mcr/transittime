9f9f60fb3dc424fa170a8686b2dc13d3c882954c
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-08-15 18:36:27 -0400

    class_name should refer to string for class, not symbol

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,10 @@
 class Place < ActiveRecord::Base
 
   has_many :transit_stops
-  has_many :starting_transits, :class_name => :transits, :foreign_key => :starting_place_id
-  has_many :ending_transits,   :class_name => :transits, :foreign_key => :ending_place_id
+  has_many :starting_transits, :class_name => "Transit", :foreign_key => :starting_place_id
+  has_many :ending_transits,   :class_name => "Transit", :foreign_key => :ending_place_id
 
-  has_many :starting_routes, :class_name => :routes, :foreign_key => :starting_place_id
-  has_many :ending_routes,   :class_name => :routes, :foreign_key => :ending_place_id
+  has_many :starting_routes, :class_name => "Route", :foreign_key => :starting_place_id
+  has_many :ending_routes,   :class_name => "Route", :foreign_key => :ending_place_id
   
 end
