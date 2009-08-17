c5ba174d86bfb7a407ae23b6a1227a712250c8dd
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-08-16 23:26:09 -0400

    correct relationship beween places and transit

--- app/models/transit.rb
+++ app/models/transit.rb
@@ -2,8 +2,8 @@ class Transit < ActiveRecord::Base
   has_many :schedule
   has_many :transit_stops, :through => :schedule
 
-  has_one :starting_place, :class_name => "Place"
-  has_one :ending_place,   :class_name => "Place"
+  belongs_to :starting_place, :class_name => "Place"
+  belongs_to :ending_place,   :class_name => "Place"
 
   belongs_to :booking
 
