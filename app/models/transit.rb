8ed2a5791d11c9bbe0935a333ef792782f839a81
Michael Richardson <mcr@sandelman.ottawa.on.ca>
2009-08-16 23:28:17 -0400

    added test case for board_at, and implementation

--- app/models/transit.rb
+++ app/models/transit.rb
@@ -7,4 +7,12 @@ class Transit < ActiveRecord::Base
 
   belongs_to :booking
 
+  def board_at(code, n = Time.now)
+    boarding = BoardingTime.new(:transit_stage => self,
+				:boardtime     => n,
+				:vehiclecode   => code)
+    boarding.transit_stop = self.starting_place.transit_stops.first
+    boarding
+  end
+
 end
---
+++ spec/models/transit_spec.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__)+'/../spec_helper'
+require 'ruby-debug'
+
+describe Transit, "with fixtures loaded" do
+  fixtures :all
+  
+  it "create a new boarding time for a given transit" do
+    tr = transits(:walktokirkwood)
+
+    boarding = tr.board_at("8088", Time.now)
+
+    assert_not_nil boarding.boardtime
+    assert_not_nil boarding.vehiclecode
+  end
+end
